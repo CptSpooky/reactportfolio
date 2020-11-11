8bbd3ee4f7d7e93892eb556853c93650a9ed8a03
CptSpooky <legacyofthedonut@gmail.com>
2020-11-11 17:41:53 -0500

    portfolio page done

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -8,15 +8,12 @@ export default function ProjectCard({ projects }) {
             {projects.map(item =>{
                 return (
                 <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="https://via.placeholder.com/180x100.png" />
+                    <Card.Img variant="top" src={item.image} />
                     <Card.Body>
                         <Card.Title>{item.title}</Card.Title>
-                        <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
-                        </Card.Text>
-                        <Button variant="primary">Deployed App</Button>
-                        <Button className="btn-margin-l" variant="secondary">Github</Button>
+                        <Card.Text>{item.description}</Card.Text>
+                        <Button variant="primary" href={item.deployed} target="blank">Deployed App</Button>
+                        <Button className="btn-margin-l" variant="secondary" href={item.github} target="blank">Github</Button>
                     </Card.Body>
                 </Card>
                 )
